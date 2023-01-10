56e7d9a40609e3316948bc6f659a017df93efaad
kateFrontend <ecdanilova@gmail.com>
2023-01-10 13:33:44 -0500

    get Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+
+  const [activity, setActivity] = useState('')
+
+  const getApi = async() => {
+    const response = await fetch(`http://www.boredapi.com/api/activity/`);
+    const data = await response.json()
+    console.log(data.activity)
+    setActivity(data.activity)
+  }
+
+  useEffect(() => {
+    getApi()
+  }, [])
+
   return (
-    <div>Get bored?</div>
+    <main>
+      <h1>Get bored?</h1>
+      <p>Click the button and get a new thing to do so you don't get bored</p>
+      <button onClick={getApi}>Click me</button>
+      <h3>{activity}</h3>
+    </main>
   )
 }
 
