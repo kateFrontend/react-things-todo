653137980e90d27b934ab444afb9ad30e4091ba0
kateFrontend <ecdanilova@gmail.com>
2023-01-10 19:23:28 -0500

    update main div

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
 
   return (
       <div className="main">
-        <h1>Get bored?</h1>
-        <p>Click the button and get a new thing to do so you don't get bored</p>
-        <button onClick={fetchApi}>Fetch Unsplash Api</button>
+        <h1>Are you bored?</h1>
+        <p>Click the button and get a new thing to do so you don't get bored again</p>
+        <button onClick={fetchApi}>Click me</button>
         <h2>{activity}</h2>
       </div>
 
