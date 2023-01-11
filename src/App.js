1a957c9fa732caf13219bdd0b4b3e1419d005ab1
kateFrontend <ecdanilova@gmail.com>
2023-01-11 14:09:03 -0500

    fix bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const fetchApi = async () => {
 
-    const response = await fetch(`http://www.boredapi.com/api/activity/`);
+    const response = await fetch(`https://www.boredapi.com/api/activity/`);
     const data = await response.json();
     console.log(data.activity);
     setActivity(data.activity);
