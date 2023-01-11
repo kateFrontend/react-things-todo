2de88795ec80676f1b13ebf5a620cf1b7647d114
kateFrontend <ecdanilova@gmail.com>
2023-01-11 13:15:44 -0500

    btn style

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <div className="main">
         <h1>Are you bored?</h1>
         <p>Click the button and get a new thing to do so you don't get bored again</p>
-        <button onClick={fetchApi}>Click me</button>
+        <button className="button-92" onClick={fetchApi}>Click me</button>
         <h2>{activity}</h2>
       </div>
 
